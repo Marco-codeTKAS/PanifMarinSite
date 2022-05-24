37892f2ae62a15f860999a04cbaf7fe5d9e8c8f8
Marco Antonio Renteria Ramirez <marcoantonioconalep@gmail.com>
2022-05-24 10:04:18 -0500

    Ajustes en layout  y Repartidores Overview

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,55 +3,25 @@
     <q-header  reveal elevated class="bg-blue-grey-1"  >
         <q-toolbar inset>
           <!-- Esto se muestra solo en movil -->
-          <q-btn
-          v-if="$q.screen.lt.sm"
-          flat
-          dense
-          round
-          color="dark"
-          class="q-mr-xl"
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-          <q-drawer
-              v-model="leftDrawerOpen"
-              :breakpoint="700"
-              elevated content-class="bg-primary" :width="200"
-            >
-              <q-scroll-area class="fit q-pt-md">
-                <div >
-                  <q-item dense clickable to="Repartidores">
-                    <q-item-section>
-                      <q-item-label class="text-h6 q-py-md"> <q-icon name="format_list_bulleted"/> Repartidores</q-item-label>
-                    </q-item-section>
-                  </q-item>
-                </div>
-                <div >
-                  <q-item dense clickable to="Perfil">
-                    <q-item-section>
-                      <q-item-label class="text-h6 q-py-md q-gutter-xs"> <q-icon name="assignment_ind " class="" /> <span>Perfil</span> </q-item-label>
-                    </q-item-section>
-                  </q-item>
-                </div>
-              </q-scroll-area>
-            </q-drawer>
-
-          <!-- <q-img
+          <MenuMobil></MenuMobil>
+          <q-img
           class="q-mr-xl"
           src="~assets/img1.svg"
           style="width:90px;"
 
-        /> -->
+        />
           <!-- Esto se muestra en escritorio -->
 
-          <q-list dense bordered padding  class="q-py-none rounded-borders desktop-only row"   v-if="$q.screen.gt.sm">
+          <q-list dense  padding class="q-py-none q-gutter-x-sm rounded-borders desktop-only row"   v-if="$q.screen.gt.sm">
             <q-item
               clickable
               v-ripple
               @click="linkActivo = 'home'"
               :active="linkActivo === 'home'"
-              active-class="my-menu-link"
+              active-class="text-white bg-primary"
+              class="text-dark"
+              style="border: 1px rgb(109, 105, 105);"
+              ref="itemEntregas"
             >
               <q-item-section avatar class="q-py-xs q-px-none">
                 <q-icon name="pin_drop" />
@@ -59,7 +29,7 @@
               <q-item-section>Entregas</q-item-section>
             </q-item>
 
-            <q-menu fit content-class="" >
+            <q-menu fit :target="$refs.itemEntregas" content-class="" >
               <q-list dense bordered padding style="min-width:140px;" class="q-pa-none rounded-borders">
                 <q-item
                   clickable
@@ -105,9 +75,50 @@
                   </q-item-section>
                   <q-item-section>Rutas</q-item-section>
                 </q-item>
+                <q-item
+                  clickable
+                  v-ripple
+                  @click="linkSubMenu = 'rutas'"
+                  :active="linkSubMenu === 'rutas'"
+                  exact-active-class="my-menu-link"
+                  active-class=""
+                  to="/Recepcion"
+                >
+                  <q-item-section avatar class="q-py-sm q-px-none">
+                    <q-icon name="assignment" size="xs"/>
+                  </q-item-section>
+                  <q-item-section>Recepcion</q-item-section>
+                </q-item>
               </q-list>
             </q-menu>
 
+            <q-item
+              clickable
+              v-ripple
+              @click="linkActivo = 'usuarios'"
+              :active="linkActivo === 'usuarios'"
+              active-class="text-white bg-primary"
+              class="text-dark "
+            >
+              <q-item-section avatar class="q-py-xs q-px-none">
+                <q-icon name="pin_drop" />
+              </q-item-section>
+              <q-item-section>Usuario</q-item-section>
+            </q-item>
+
+             <q-item
+              clickable
+              v-ripple
+              @click="linkActivo = 'pan'"
+              :active="linkActivo === 'pan'"
+              active-class="text-white bg-primary"
+              class="text-dark "
+            >
+              <q-item-section avatar class="q-py-xs q-px-none">
+                <q-icon name="pin_drop" />
+              </q-item-section>
+              <q-item-section>Pan</q-item-section>
+            </q-item>
           </q-list>
 
            <q-toolbar-title></q-toolbar-title>
@@ -126,14 +137,15 @@
 </template>
 
 <script>
+import MenuMobil from '../components/MenuMobil.vue'
 export default {
   name: 'MainLayout',
   components: {
-
+    MenuMobil
   },
   data () {
     return {
-      leftDrawerOpen: false,
+
       btnToggle: 'Home',
       linkActivo: 'home',
       linkSubMenu: ''
@@ -154,6 +166,7 @@ export default {
   .my-menu-link{
     background-color: #1976D2;
     color:white;
+    border: 1px solid rgb(109, 105, 105);
 
   }
 </style>
--- src/pages/RepartidorOverview.vue
+++ src/pages/RepartidorOverview.vue
@@ -2,7 +2,7 @@
     <q-page>
         <q-card class="row q-my-none">
           <q-card-section>
-              <h4 class="text-h6 q-mx-md q-my-xs" style="text-align:center;">Nombre Repartidor</h4>
+              <h4 class="text-h6 q-mx-md q-my-xs" style="text-align:center;">{{}}</h4>
           </q-card-section>
           <q-card-section class="row full-width q-py-none items-baseline justify-around"  >
               <h4 class="text-subtitle1 q-mt-sm q-mb-none" style="text-align:center;">Realiza tu pedido</h4>
@@ -20,20 +20,29 @@
 </template>
 
 <script>
+
 export default {
   name: 'RepartidorOverview',
+
   data () {
     return {
       Campos: ['Chinos', 'Bolillo6', 'Bolillo3', 'Telera4', 'TrancaG', 'ConchaG', 'PiconG', 'BolilloG', 'Avena', 'AvenaG', 'AvenaCH'],
-      modelInputCampo: []
-
+      modelInputCampo: [],
+      dataRepartidor: null
     }
   },
-  created () {
-
+  mounted () {
+    console.log(this.dataRepartidores)
   },
   methods: {
+    GuardarData () {
 
+    },
+    DatosRepartidor () {
+      this.$axios.get('Repartidores').then(res => {
+        this.dataRepartidor = res.data
+      })
+    }
   }
 }
 </script>
